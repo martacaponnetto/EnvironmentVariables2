7812361884b348edff3987bc74d73f25608287d4
martacaponnetto <martacaponnetto@yahoo.it>
2024-05-27 14:24:16 +0200

    first commit

---
+++ src/main/java/com/example/demoEnvironmentVariables2/DemoEnvironmentVariables2Application.java
@@ -0,0 +1,13 @@
+package com.example.demoEnvironmentVariables2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEnvironmentVariables2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEnvironmentVariables2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demoEnvironmentVariables2/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.example.demoEnvironmentVariables2.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @Value("${welcomeMsg}")
+    private String welcomeMsg;
+
+    @GetMapping("/")
+    public String getMessage() {
+        return welcomeMsg;
+    }
+}
